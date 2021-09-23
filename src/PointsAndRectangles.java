1cd16ebc248d2e4f898924cdc2f84d3f3dd479fc
simmsju <simmsju@ssfs.org>
2021-09-23 14:51:59 -0400

    Initial Unit Two with Points and Rectangles

--- src/PointsAndRectangles.java
+++ src/PointsAndRectangles.java
@@ -10,9 +10,21 @@ public class PointsAndRectangles
     {
         Scanner kb = new Scanner(System.in);
         // create print statements and get input for the x and y values.
-
+        System.out.print("Enter the x coordinate: ");
+        int x = kb.nextInt();
+        System.out.print("Enter the y coordinate: ");
+        int y = kb.nextInt();
+        System.out.print("Enter the width: ");
+        int width = kb.nextInt();
+        System.out.print("Enter the height: ");
+        int height = kb.nextInt();
         //Instantiate a Point object p. Hint: you will need the keyword "new"
-
+        Point p1 = new Point(x, y);
+        Dimension d1 = new Dimension(width, height);
+        Rectangle r1 = new Rectangle(p1, d1);
+        System.out.println(p1);
+        System.out.println(d1);
+        System.out.println(r1);
 
         // create print statements and get input for the length and width
 
